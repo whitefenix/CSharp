57ce5cc2ea06997918d545e63f2f3b9d7fba2457
whitefenix <samuel.ekne@gmail.com>
2016-11-10 15:28:38 +0100

    Alpha version of UI dont look

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+
+    public Texture ViolinExample, ViolinExample2;
+    [SerializeField]
+    string mainEquip, secondEquip;
+
+
+    void Update()
+    {
+        InputHandler();
+    }
+
+    void InputHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.G) == true)
+        {
+            Debug.Log("G");
+        }
+        else if (Input.GetKeyDown(KeyCode.H) == true)
+        {
+            Debug.Log("H");
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(100, Screen.height - 200, 200, 200), ViolinExample, GUIStyle.none);
+        GUI.Box(new Rect(Screen.width - 300, Screen.height - 200, 200, 200), ViolinExample, GUIStyle.none);
+
+    }
+}
