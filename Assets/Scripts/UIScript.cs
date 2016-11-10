25bb668daf34ffaaf1145b5fd60627557859ba6d
whitefenix <samuel.ekne@gmail.com>
2016-11-10 16:00:47 +0100

    More UI, G or H now opens second menu

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UIScript : MonoBehaviour {
+public class UIScript : MonoBehaviour
+{
 
-    public Texture ViolinExample, ViolinExample2;
-    [SerializeField]
-    string mainEquip, secondEquip;
+    public Texture violinExample, harpExample, mainHand, offHand;
 
+    private bool mainHandMenu = false;
+    private bool offHandMenu = false;
+
+    private int mainMenuPos = 0, offHandMenuPos = 0;
 
     void Update()
     {
@@ -17,18 +20,73 @@ public class UIScript : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.G) == true)
         {
-            Debug.Log("G");
+            //I hate reading one line ifs. If you want them you can change this.
+            //Turn menu on if it was off, close it if it was on
+            if (mainHandMenu == false)
+            {
+                mainHandMenu = true;
+            }
+            else
+            {
+                mainHandMenu = false;
+            }
+
+            if (mainHand == violinExample)
+            {
+                mainHand = harpExample;
+            }
+            else
+            {
+                mainHand = violinExample;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.H) == true)
         {
-            Debug.Log("H");
+            //Turn menu on if it was off, close it if it was on
+            if (offHandMenu == false)
+            {
+                offHandMenu = true;
+            }
+            else
+            {
+                offHandMenu = false;
+            }
+            if (offHand == violinExample)
+            {
+                offHand = harpExample;
+            }
+            else
+            {
+                offHand = violinExample;
+            }
         }
     }
 
     void OnGUI()
     {
-        GUI.Box(new Rect(100, Screen.height - 200, 200, 200), ViolinExample, GUIStyle.none);
-        GUI.Box(new Rect(Screen.width - 300, Screen.height - 200, 200, 200), ViolinExample, GUIStyle.none);
+        //main hand
+        GUI.Box(new Rect(100, Screen.height - 200, 200, 200), mainHand, GUIStyle.none);
 
+        //off hand
+        GUI.Box(new Rect(Screen.width - 300, Screen.height - 200, 200, 200), offHand, GUIStyle.none); 
+
+        if (mainHandMenu)
+        {
+            GUI.Box(new Rect(50, Screen.height - 250, 300, 75), "");
+        }
+
+        if (offHandMenu)
+        {
+            GUI.Box(new Rect(Screen.width - 350, Screen.height - 250, 300, 75), "");
+        }
+
+        //hearts
+        /*
+        GUI.Box(new Rect(Screen.width - 375, Screen.height - 125, 50, 50), "");
+        GUI.Box(new Rect(Screen.width - 475, Screen.height - 125, 50, 50), "");
+        GUI.Box(new Rect(Screen.width - 575, Screen.height - 125, 50, 50), "");
+        GUI.Box(new Rect(Screen.width - 675, Screen.height - 125, 50, 50), "");
+        GUI.Box(new Rect(Screen.width - 775, Screen.height - 125, 50, 50), "");
+        */
     }
 }
