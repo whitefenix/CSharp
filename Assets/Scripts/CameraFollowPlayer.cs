bf4d5bf95398e2f6469e389164eded407ebdbd36
Hansjörg Hofer <hansjoerg.h.89@gmail.com>
2016-11-15 21:53:59 +0100

    detached camera from player, added world boundaries for demo

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 offset;
+
+	public float minX = -100.0f;
+	public float minZ = -100.0f;
+	public float maxX = 100.0f;
+	public float maxZ = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+		player = GameObject.FindWithTag ("Player").transform;
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		Vector3 newPosition = player.position;
+		newPosition.y = 0;
+		newPosition += offset;
+
+		newPosition.x = Mathf.Clamp (newPosition.x, minX, maxX);
+		newPosition.z = Mathf.Clamp (newPosition.z, minZ, maxZ);
+
+		//transform.position = Vector3.Lerp (transform.position, newPosition, (transform.position - newPosition).magnitude);
+
+		transform.position = newPosition;
+	}
+}
