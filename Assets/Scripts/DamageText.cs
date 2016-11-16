e6c015e64ea1f1e60c253d52aca9d5c41b165088
Hansjörg Hofer <hansjoerg.h.89@gmail.com>
2016-11-16 18:46:32 +0100

    added fancy damage labels

--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -4,17 +4,29 @@ using System.Collections;
 public class DamageText : MonoBehaviour {
 
 	public float lifespan = 1.0f;
-	public float speed = 0.1f;
+	public float movementSpeed = 0.1f;
+	public float growingSpeed = 0.1f;
+	public int sortingOrder = 100;
 
 	public Vector3 spawnPoint = new Vector3 (0, 3, 0);
+
 	private Vector3 translation;
+	private Color transparency;
+	private TextMesh text;
+	private float fontSize;
 
 	// Use this for initialization
 	void Start () 
 	{
-		transform.position += spawnPoint;
+		text = GetComponent<TextMesh> ();
 
-		translation = new Vector3 (0, speed, 0);
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+		mr.sortingOrder = sortingOrder;
+
+		translation = new Vector3 (0, movementSpeed, 0);
+		transparency = new Color (0, 0, 0, growingSpeed);
+
+		transform.position += spawnPoint;
 
 		GameObject.Destroy (gameObject, lifespan);
 	}
@@ -23,5 +35,10 @@ public class DamageText : MonoBehaviour {
 	void FixedUpdate () 
 	{
 		gameObject.transform.position += translation;
+
+		fontSize += growingSpeed;
+		text.fontSize += Mathf.RoundToInt (fontSize);
+
+		text.color -= transparency;
 	}
 }
