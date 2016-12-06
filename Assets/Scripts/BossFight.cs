e496140a7851fe63b446f514cb2efcaf9dd56871
whitefenix <samuel.ekne@gmail.com>
2016-12-06 17:46:42 +0100

    boss sprite and prefabs

---
+++ Assets/Scripts/BossFight.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFight : MonoBehaviour {
+
+    Health health;
+    public GameObject littleBoss;
+    private bool hasSplit = false;
+
+    public float waitTimeBeforeSplit = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	 health = GetComponent<Health>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health.currentHealth < health.maximumHealth / 2)
+        {
+          
+            if (!hasSplit)
+            {
+                StartCoroutine(split());               
+            }
+           
+            //trigger split
+            //destroy
+        }
+	
+	}
+
+    IEnumerator split()
+    {
+        Debug.Log("Start");
+        health.currentHealth = health.maximumHealth; //just so he doesnt die while transforming
+        //TODO: Stop + particle effect
+        yield return new WaitForSeconds(waitTimeBeforeSplit); //wait for 1 second before spawning
+        Debug.Log("Go");
+        Vector3 position = transform.position;
+        GameObject littleBoss1 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        position.x -= 2;
+        GameObject littleBoss2 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        position.z -= 2;
+        GameObject littleBoss3 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        position.x += 2;
+        GameObject littleBoss4 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        position.x += 2;
+        GameObject littleBoss5 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        position.z += 2;
+        GameObject littleBoss6 = (GameObject)Instantiate(littleBoss, position, Quaternion.identity);
+        hasSplit = true;
+        GameObject.Destroy(gameObject);
+    }
+}
