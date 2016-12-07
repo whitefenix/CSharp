e9183fa9962eba7d671d6edcb38f8d1cb0f5bcb8
whitefenix <samuel.ekne@gmail.com>
2016-12-07 19:33:55 +0100

    video scene and script

---
+++ Assets/Scripts/VideoScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoScript : MonoBehaviour {
+
+    public MovieTexture test;
+    AudioSource source;
+
+    void Start()
+    {      
+        source = GetComponent<AudioSource>();
+        source.clip = test.audioClip;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+
+            if (test.isPlaying)
+            {
+                source.Pause();
+                test.Pause();
+            }
+            else
+            {
+                test.Play();
+                source.Play();
+            }
+        }
+    }
+
+}
