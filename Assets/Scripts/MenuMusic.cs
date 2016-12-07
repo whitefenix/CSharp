d51bcdcc4db3a51fee7ce1d2bc8718f99fa1ccc1
whitefenix <samuel.ekne@gmail.com>
2016-12-07 19:53:49 +0100

    menu music

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+	}
+}
